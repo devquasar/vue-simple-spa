0e61507e78bdcdd92560090ce989365c9d8a6fdd
rkz1337 <booster2228@yandex.ru>
2020-09-11 00:44:38 +0300

    Add Create Task form WIP

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+import M from "materialize-css";
 import Navbar from "@/components/Navbar";
 export default {
   components: { Navbar },
@@ -16,4 +17,7 @@ export default {
 
 <style lang="scss">
 @import "~materialize-css/dist/css/materialize.min.css";
+body {
+  font-family: "Montserrat", "Segoe UI", sans-serif;
+}
 </style>
--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,12 +1,49 @@
 <template>
-  <div>
-    <h1>Hello</h1>
+  <div class="row">
+    <div class="col s6 offset-s3">
+      <h1>Create Task</h1>
+      <form>
+        <div class="input-field">
+          <input
+            v-model="title"
+            id="title"
+            type="text"
+            class="validate"
+            required
+          />
+          <label for="title">Title</label>
+          <span class="helper-text" data-error="Title is required"></span>
+        </div>
+
+        <div class="chips mb-4" ref="chips"></div>
+
+        <div class="input-field">
+          <textarea
+            v-model="description"
+            id="description"
+            class="materialize-textarea"
+          ></textarea>
+          <label for="description">Description</label>
+          <span class="character-counter" style="float: right; font-size: 12px;"
+            >{{ description.length }}/2048</span
+          >
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Create",
-  components: {},
+  name: "create",
+  data: () => ({
+    description: "",
+    title: "",
+  }),
+  mounted() {
+    M.Chips.init(this.$refs.chips, {
+      placeholder: "Add tags :)",
+    });
+  },
 };
 </script>
