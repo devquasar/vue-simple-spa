4d43507746574fb614691f3abcdf1df5b9cee3a2
rkz1337 <booster2228@yandex.ru>
2020-09-11 21:57:53 +0300

    Work on Create page

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -2,32 +2,25 @@
   <div class="row">
     <div class="col s6 offset-s3">
       <h1>Create Task</h1>
-      <form>
+      <form @submit.prevent="submitHandler">
         <div class="input-field">
-          <input
-            v-model="title"
-            id="title"
-            type="text"
-            class="validate"
-            required
-          />
+          <input v-model="title" id="title" type="text" class="validate" required />
           <label for="title">Title</label>
           <span class="helper-text" data-error="Title is required"></span>
         </div>
 
         <div class="chips mb-4" ref="chips"></div>
-
         <div class="input-field">
-          <textarea
-            v-model="description"
-            id="description"
-            class="materialize-textarea"
-          ></textarea>
+          <textarea v-model="description" id="description" class="materialize-textarea"></textarea>
           <label for="description">Description</label>
-          <span class="character-counter" style="float: right; font-size: 12px;"
-            >{{ description.length }}/2048</span
-          >
+          <span
+            class="character-counter"
+            style="float: right; font-size: 12px;"
+          >{{ description.length }}/2048</span>
         </div>
+
+        <input type="text" ref="datepicker" />
+        <button class="btn" type="submit">Create Task</button>
       </form>
     </div>
   </div>
@@ -36,14 +29,48 @@
 <script>
 export default {
   name: "create",
+
   data: () => ({
     description: "",
     title: "",
+    chips: null,
+    date: null,
   }),
+
   mounted() {
-    M.Chips.init(this.$refs.chips, {
+    this.chips = M.Chips.init(this.$refs.chips, {
       placeholder: "Add tags :)",
     });
+    this.date = M.Datepicker.init(this.$refs.datepicker, {
+      format: "dd.mm.yyyy",
+      defaultDate: new Date(),
+      setDefaultDate: true,
+    });
+  },
+
+  methods: {
+    submitHandler() {
+      const task = {
+        title: this.title,
+        description: this.description,
+        id: Date.now(),
+        status: "active",
+        tags: this.chips.chipsData,
+        date: this.date.date,
+      };
+      this.$store.dispatch("createTask", task);
+      this.$router.push("/list");
+    },
+  },
+
+  destroyed() {
+    if (this.date && this.date.destroy) {
+      this.date.destroy();
+    }
+
+    if (this.chips && this.chips.destroy) {
+      this.chips.destroy();
+    }
   },
 };
 </script>
